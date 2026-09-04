2f2ff3372c1dac82230ea80b0adb53a0edbfd448
shoenfelte <shoenfelte@vt.edu>
2026-09-03 21:05:21 -0400

    Update Prompt.cpp

--- Prompt.cpp
+++ Prompt.cpp
@@ -1,41 +1,33 @@
 #include "Prompt.hpp"
 
-Prompt::Prompt(std::string title, std::string text) {
-    // TODO: implement according to the M1 specification.
-
+Prompt::Prompt(std::string title, std::string text)
+    : title_(std::move(title)), text_(std::move(text)) {
 }
 
 bool Prompt::operator==(const Prompt& other) const {
-    // TODO
-
+    return title_ == other.title_ && text_ == other.text_;
 }
 
 bool Prompt::operator!=(const Prompt& other) const {
-    // TODO
-
+    return !(*this == other);
 }
 
 const std::string& Prompt::title() const noexcept {
-    // TODO
-
+    return title_;
 }
 
 const std::string& Prompt::text() const noexcept {
-    // TODO
-
+    return text_;
 }
 
 void Prompt::setTitle(std::string title) {
-    // TODO
-
+    title_ = std::move(title);
 }
 
 void Prompt::setText(std::string text) {
-    // TODO
-
+    text_ = std::move(text);
 }
 
 bool Prompt::empty() const noexcept {
-    // TODO
-
+    return text_.empty();
 }
