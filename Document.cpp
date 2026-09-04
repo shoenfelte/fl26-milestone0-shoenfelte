30e36c7461f6da356ecd8c70117229c6e3ff5338
shoenfelte <shoenfelte@vt.edu>
2026-09-03 20:59:23 -0400

    Update Document.cpp

--- Document.cpp
+++ Document.cpp
@@ -1,51 +1,66 @@
 #include "Document.hpp"
 
-Document::Document(std::string title, std::string contents) {
-    // TODO: implement according to the M1 specification.
+#include <fstream>
+#include <sstream>
 
+Document::Document(std::string title, std::string contents)
+    : title_(std::move(title)), sourcePath_(), contents_(std::move(contents)) {
 }
 
 bool Document::operator==(const Document& other) const {
-    // TODO
-
+    return title_ == other.title_ &&
+           sourcePath_ == other.sourcePath_ &&
+           contents_ == other.contents_;
 }
 
 bool Document::operator!=(const Document& other) const {
-    // TODO
-
+    return !(*this == other);
 }
 
 bool Document::load(const std::string& path) {
-    // TODO
+    std::ifstream file(path, std::ios::binary);
+    if (!file) {
+        return false;
+    }
 
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    if (!file.good() && !file.eof()) {
+        return false;
+    }
+
+    // extract  final filename component from the path (handles '/' and '\\')
+    std::size_t lastSlash = path.find_last_of("/\\");
+    std::string filename =
+        (lastSlash == std::string::npos) ? path : path.substr(lastSlash + 1);
+
+    sourcePath_ = path;
+    title_ = filename;
+    contents_ = buffer.str();
+    return true;
 }
 
 const std::string& Document::title() const noexcept {
-    // TODO
-
+    return title_;
 }
 
 const std::string& Document::sourcePath() const noexcept {
-    // TODO
-
+    return sourcePath_;
 }
 
 const std::string& Document::contents() const noexcept {
-    // TODO
-  
+    return contents_;
 }
 
 void Document::setTitle(std::string title) {
-    // TODO
-  
+    title_ = std::move(title);
 }
 
 std::size_t Document::characterCount() const noexcept {
-    // TODO
-    
+    return contents_.size();
 }
 
 bool Document::empty() const noexcept {
-    // TODO
-   
+    return contents_.empty();
 }
+
